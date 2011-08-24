096d72bcc787eb22281d5c26bc3cba48d5f0f99b
pgengler <pgengler@5e5b0a04-d1f8-4ac3-904a-ef7cfcaa6b43>
2011-08-24 19:39:17 +0000

    Remove disconnected clients from list
    git-svn-id: file:///srv/svn/crossword/trunk@9 5e5b0a04-d1f8-4ac3-904a-ef7cfcaa6b43

--- server/server.js
+++ server/server.js
@@ -3,16 +3,25 @@ var io = require('socket.io').listen(2000);
 
 var clients = [ ];
 
+io.configure(function() {
+	io.set('log level', 1);
+});
+
 io.sockets.on('connection', function(socket) {
 
 	clients.push(socket);
 
 	socket.on('play', function(data) {
 
-		var clientsToNotify = _.filter(clients, function(s) { return s != socket });
+		var clientsToNotify = _(clients).filter(function(s) { return s != socket });
 
 		_.each(clientsToNotify, function(s) {
 			s.emit('play', { x: data.x, y: data.y, letter: data.letter });
 		});
 	});
+
+	socket.on('disconnect', function() {
+		clients = _(clients).filter(function(client) { return socket != client });
+	});
 });
+
