42421e015aeec410d194a1e383301ecab77f6115
pgengler <pgengler@5e5b0a04-d1f8-4ac3-904a-ef7cfcaa6b43>
2011-08-25 20:19:40 +0000

    Store board state (#1) and transmit to new clients (#2)
    git-svn-id: file:///srv/svn/crossword/trunk@10 5e5b0a04-d1f8-4ac3-904a-ef7cfcaa6b43

--- server/server.js
+++ server/server.js
@@ -3,17 +3,26 @@ var io = require('socket.io').listen(2000);
 
 var clients = [ ];
 
+var board = [ ];
+
 io.configure(function() {
 	io.set('log level', 1);
 });
 
 io.sockets.on('connection', function(socket) {
 
+	socket.emit('board', { board: board });
+
 	clients.push(socket);
 
 	socket.on('play', function(data) {
 
 		var clientsToNotify = _(clients).filter(function(s) { return s != socket });
+
+		if (typeof board[data.y] === 'undefined') {
+			board[data.y] = [ ];
+		}
+		board[data.y][data.x] = data.letter;
 
 		_.each(clientsToNotify, function(s) {
 			s.emit('play', { x: data.x, y: data.y, letter: data.letter });
